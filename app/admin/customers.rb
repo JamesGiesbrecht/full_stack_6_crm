de9e72338da0bb11b0ae9d4d62ce485964300049
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-10-31 14:13:17 -0500

    Attaching image and name validation to customer

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -5,7 +5,19 @@ ActiveAdmin.register Customer do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :name, :phone, :email, :notes
+  permit_params :name, :phone, :email, :notes, :image
+
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs # builds an input field for every attribute
+    # start custom section
+    f.inputs do
+      f.input :image, as: :file # adding image file upload
+    end
+    # end custom section
+    f.actions # adds the 'Submit' and 'Cancel' buttons
+  end
+
   #
   # or
   #
